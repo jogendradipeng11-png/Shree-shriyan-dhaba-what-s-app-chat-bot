ea14afd4182e232006fa045ce3f7747eaf39d405
JOGENDRA PATEL <jogendra.dipeng11@gmail.com>
2026-04-14 07:53:15 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,17 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "dhaba-bills",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : '',
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -8,3 +20,5 @@ admin.initializeApp({
 
 const db = admin.database();
 module.exports = db;
+
+console.log("✅ Firebase connected successfully using Render environment variables");

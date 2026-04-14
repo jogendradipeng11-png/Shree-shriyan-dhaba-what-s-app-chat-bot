e26ad9dbc38f651c6071da02ea5916cd16f0dfba
JOGENDRA PATEL <jogendra.dipeng11@gmail.com>
2026-04-14 21:34:33 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY 
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') 
-    : '',
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -21,6 +8,7 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-module.exports = db;
 
-console.log("✅ Firebase connected successfully using Render environment variables");
+console.log("✅ Firebase Connected");
+
+module.exports = { db };
